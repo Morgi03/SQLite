a4a875b0cc2b55318591f94a4412224e69f80809
Morgován Nimród <morgovannimrod22@gmail.com>
2022-12-01 15:29:13 +0100

    Rögzítés és adat Olvasás megírása

--- app/src/main/java/hu/petrik/sqlite/DataRead.java
+++ app/src/main/java/hu/petrik/sqlite/DataRead.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class DataRead extends AppCompatActivity {
 
@@ -15,15 +16,33 @@ public class DataRead extends AppCompatActivity {
     private Button rogzitesBtn;
     private Button visszaBtn;
 
+    private DBHelper adatbazis;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_read);
         init();
         visszaBtn.setOnClickListener(view -> {
-                Intent intent = new Intent(this, MainActivity.class);
-                startActivity(intent);
-                finish();
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        rogzitesBtn.setOnClickListener(view -> {
+            String vezeteknev = vezeteknevEditText.getText().toString().trim();
+            String keresztnev = keresztnevEditText.getText().toString().trim();
+            String jegyString = jegyEditText.getText().toString().trim();
+            if (vezeteknev.isEmpty() || jegyString.isEmpty() || keresztnev.isEmpty()) {
+                Toast.makeText(this, "Minden mező kitöltése kötelező", Toast.LENGTH_SHORT).show();
+            } else {
+                int jegy = Integer.parseInt(jegyString);
+                if (adatbazis.rogzites(vezeteknev, keresztnev, jegy)) {
+                    Toast.makeText(this, "Sikeres adatrögzítés", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Sikertelen adatrögzítés", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 
@@ -33,5 +52,6 @@ public class DataRead extends AppCompatActivity {
         jegyEditText = findViewById(R.id.jegyEditText);
         rogzitesBtn = findViewById(R.id.rogzitesBtn);
         visszaBtn = findViewById(R.id.visszaBtn);
+        adatbazis = new DBHelper(DataRead.this);
     }
 }
--- app/src/main/java/hu/petrik/sqlite/MainActivity.java
+++ app/src/main/java/hu/petrik/sqlite/MainActivity.java
@@ -3,9 +3,11 @@ package hu.petrik.sqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +17,34 @@ public class MainActivity extends AppCompatActivity {
     private Button adatTorlese;
     private TextView adatokTextView;
 
+    private DBHelper adatbazis;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+
+        adatokOlvasasaBtn.setOnClickListener(view -> {
+            Cursor adatok = adatbazis.listaz();
+            if (adatok.getCount() == 0) {
+                Toast.makeText(this, "Nincs az adatbázisban bejegyzés", Toast.LENGTH_SHORT).show();
+            } else {
+                StringBuilder bobTheBuilder = new StringBuilder();
+                while (adatok.moveToNext()) {
+                    bobTheBuilder.append("ID: ").append(adatok.getInt(0));
+                    bobTheBuilder.append(System.lineSeparator());
+                    bobTheBuilder.append("Vezetéknév: ").append(adatok.getString(1));
+                    bobTheBuilder.append(System.lineSeparator());
+                    bobTheBuilder.append("Keresztnév: ").append(adatok.getString(2));
+                    bobTheBuilder.append(System.lineSeparator());
+                    bobTheBuilder.append("Jegy: ").append(adatok.getInt(3));
+                    bobTheBuilder.append(System.lineSeparator());
+                    bobTheBuilder.append(System.lineSeparator());
+                }
+                adatokTextView.setText(bobTheBuilder.toString());
+            }
+        });
 
         adatokRogziteseBtn.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this, DataRead.class);
@@ -34,5 +59,6 @@ public class MainActivity extends AppCompatActivity {
         adatokModositasaBtn = findViewById(R.id.adatokModositasaBtn);
         adatTorlese = findViewById(R.id.adatTorlese);
         adatokTextView = findViewById(R.id.adatokTextView);
+        adatbazis = new DBHelper(MainActivity.this);
     }
 }
